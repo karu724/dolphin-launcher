c681b1d7b07e9142f4d16cdacd2bfcfc7fa69792
Spotky1004 <spotky1004p@gmail.com>
2022-12-25 23:48:35 +0900

    Initial commit

---
+++ app/assets/js/scripts/directlink.js
@@ -0,0 +1,15 @@
+const cafe = document.getElementById('cafe')
+const discord = document.getElementById('discord')
+const minepg = document.getElementById('minepg')
+
+discord.onclick = () => {
+    shell.openExternal('https://discord.gg/mcdolphin')
+}
+
+minepg.onclick = () => {
+    shell.openExternal('https://minelist.kr/servers/mcdolphin.kr')
+}
+
+cafe.onclick = () => {
+    shell.openExternal('https://cafe.naver.com/reda7s2x/226')
+}
